5521a3677f2efa327f0d2236092ce178b6ce00f9
Matt Beckett <beck24@gmail.com>
2018-12-21 23:36:12 -0800

    working server

--- src/app/setup-timer/setup-timer.page.ts
+++ src/app/setup-timer/setup-timer.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-setup-timer',
@@ -6,10 +7,78 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./setup-timer.page.scss'],
 })
 export class SetupTimerPage implements OnInit {
+  serverStarted: boolean = false;
+  loading = null;
+  loadingMessage = 'Checking wifi';
 
-  constructor() { }
+  constructor(
+    public loadingController: LoadingController
+  ) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    // this.loading = await this.loadingController.create({
+    //   message: this.loadingMessage,
+    // });
+
+    // this.loading.present();
+
+    // this.startServer();
   }
 
+  startServer() {
+    const webserver = window['webserver'];
+
+    if (!webserver) {
+      console.log('webserver unavailable');
+      return;
+    }
+
+    webserver.onRequest(
+      (request) => {
+        webserver.sendResponse(
+          request.requestId,
+          {
+            status: 200,
+            body: '<html>Hello World</html>',
+            headers: {
+              'Content-Type': 'text/html'
+            }
+          }
+        );
+      }
+    );
+
+    const s = () => {
+      console.log('started');
+      this.serverStarted = true;
+    };
+
+    const e = (er) => {
+      console.log('errored');
+      console.log(er);
+    };
+    
+    webserver.start(s, e, 8002);
+  }
+
+  stopServer() {
+    const webserver = window['webserver'];
+
+    if (!webserver) {
+      console.log('webserver unavailable');
+      return;
+    }
+
+    const success = () => {
+      console.log('stopped!');
+      this.serverStarted = false;
+    }
+
+    const error = (err) => {
+      console.log('failed to stop server');
+      console.log(err);
+    }
+
+    webserver.stop(success, error);
+  }
 }
